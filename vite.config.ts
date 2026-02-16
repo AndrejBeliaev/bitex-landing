f6bf334e3a1123890812a728de5d49d9d7306150
Андрей Беляев <andrejbelaev@MacBook-Air-Andrej.local>
2026-02-17 01:36:49 +0500

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -121,6 +121,43 @@ export default defineConfig({
             attrs: { name: 'theme-color', content: '#0f172a' },
             injectTo: 'head',
           },
+          {
+            tag: 'script',
+            attrs: { type: 'text/javascript' },
+            children: `
+              (function(m,e,t,r,i,k,a){
+                  m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                  m[i].l=1*new Date();
+                  for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                  k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106860837', 'ym');
+    
+              ym(106860837, 'init', {
+                ssr:true,
+                webvisor:true,
+                clickmap:true,
+                ecommerce:"dataLayer",
+                referrer: document.referrer,
+                url: location.href,
+                accurateTrackBounce:true,
+                trackLinks:true
+              });
+            `,
+            injectTo: 'head',
+          },
+    
+          // === NoScript fallback (BODY) ===
+          {
+            tag: 'noscript',
+            children: `
+              <div>
+                <img src="https://mc.yandex.ru/watch/106860837"
+                     style="position:absolute; left:-9999px;"
+                     alt="" />
+              </div>
+            `,
+            injectTo: 'body',
+          },
         ]
       },
     },
